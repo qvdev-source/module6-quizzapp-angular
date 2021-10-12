450c8417cb07296cd356dbc48e67371c42acd038
qv92 <hienquoc412018@gmail.com>
2021-10-12 14:55:27 +0700

    update commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,6 +10,7 @@ import {Router} from "@angular/router";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
 
   user : User = new User();
   faUser = faUserCircle;
